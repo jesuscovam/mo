fdb90b665c1cfb830969375a75e1f669cc9bdeef
Jesus Cova <jesuscovam@gmail.com>
2023-09-10 14:32:22 -0600

    removi paquetes sin usar

--- apps/jesus/src/lib/components/Sections/TechIcon/TechIcon.svelte
+++ apps/jesus/src/lib/components/Sections/TechIcon/TechIcon.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
-	import { TechStack } from '../../../types';
-	import graphql from './graphql.svg?url';
-	import reactNative from './react-native-logo.png';
-	import s3 from './s3.png';
-	import svelte from './svelte.svg?url';
+  import { TechStack } from '../../../types';
+  import graphql from './graphql.svg?url';
+  import reactNative from './react-native-logo.png';
+  import postgres from './postgres.svg?url';
+  import s3 from './s3.png';
+  import svelte from './svelte.svg?url';
 
-	export let tech: TechStack = TechStack.GraphQL;
+  export let tech: TechStack = TechStack.GraphQL;
 
-	$: image =
-		tech === TechStack.GraphQL
-			? graphql
-			: tech === TechStack.SvelteKit
-			? svelte
-			: tech === TechStack.S3
-			? s3
-			: reactNative;
+  $: image =
+    tech === TechStack.GraphQL
+      ? graphql
+      : tech === TechStack.SvelteKit
+      ? svelte
+      : tech === TechStack.S3
+      ? s3
+      : tech === TechStack.ReactNative
+      ? reactNative
+      : postgres;
 </script>
 
-<img src={image} alt={String(tech)} class="w-4 h-4" />
+<img src={image} alt={String(tech)} class="w-6 h-6" />
--- apps/jesus/src/lib/types.ts
+++ apps/jesus/src/lib/types.ts
@@ -2,7 +2,8 @@ export enum TechStack {
   SvelteKit,
   ReactNative,
   GraphQL,
-  S3
+  S3,
+  Postgres
 }
 
 export type Layout = 'grid' | 'flex';
