2d2859358aef2fa5c702b6282aa8d7b78dcda5bb
Jesus Cova <jesuscovam@gmail.com>
2024-06-07 03:35:25 -0600

    links  added

--- apps/jesus/src/routes/music/+layout.server.ts
+++ apps/jesus/src/routes/music/+layout.server.ts
@@ -6,9 +6,9 @@ const releases: Release[] = [
     title: 'KUROKO ABRIENDO KANCHA & SIDEKICK FRIENDSHIP',
     image: Kuroko,
     key: 'Kuroko',
-    spotify: '',
-    apple: '',
-    ytmusic: ''
+    spotify: 'https://open.spotify.com/album/0EoeHKp2C3JlJcdV5LkZno?si=6hoMlpEnSlmZWf78Bjh9Iw',
+    apple: 'https://music.apple.com/us/album/kuroko-abriendo-kancha-sidekick-friendship-single/1748855593',
+    ytmusic: 'https://music.youtube.com/playlist?list=OLAK5uy_m3B8LnqpAdHTwxOUNRSDFZRuKRWKkdey0&si=6akiqMfp2akMkLz_'
   }
 ];
 
